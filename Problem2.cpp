3db8bc1f09f85755e918be4852dc32178ea33646
shazmaan <shazmaan@buffalo.edu>
2019-07-11 01:06:30 +0400

    found a logic similar to mine.

--- Problem2.cpp
+++ Problem2.cpp
@@ -15,32 +15,54 @@ using namespace std;
 
 class Solution{
 public:
-    int prob(vector<int>& nums){
-        int index = 2; int sum=nums[0]; vector<int> allNum;
-        while(index<nums.size()){
-            if(index+2<nums.size()){
-                if(nums[index+1]>nums[index]){
-                    if(nums[index+1]<nums[index+2]){
-                        sum+=nums[index]; allNum.push_back(nums[index]);
-                    }else{sum+=nums[index+1]; allNum.push_back(nums[index+1]);}
-                }else{sum+=nums[index];allNum.push_back(nums[index]);}
-            }else if(index+1<nums.size()){
-                if(nums[index+1]>nums[index]){
-                    sum+=nums[index+1]; allNum.push_back(nums[index+1]);
-                }else{sum+=nums[index];allNum.push_back(nums[index]);}
-            }else{
-                if(index-1>=0 && nums[index-1]>nums[index] && find(allNum.begin(),allNum.end(),nums[index-1])==allNum.end()){
-                    sum+=nums[index];
-                }else if(index-2>=0 && find(allNum.begin(),allNum.end(),nums[index-2])!=allNum.end()){
-                        sum+=nums[index];
-                }
-//                }
-            }
-            cout<<nums[index]<<" "<<sum<<endl;
-            index+=2;
+    int rob(vector<int>& nums){
+        int size= nums.size();
+        if (size == 0){return 0;}
+
+        int ind1 = nums.at(0);
+        if (size == 1){return ind1;}
+
+        int ind2 = max(nums.at(0), nums.at(1));
+        if (size == 2){return ind2;}
+
+        int maxVal;
+
+        // Fill remaining positions
+        for (int i=2; i<size; i++)
+        {
+            maxVal = max(nums.at(i)+ind1, ind2);
+            ind1 = ind2;
+            ind2 = maxVal;
         }
-        return sum;
+
+        return maxVal;
     }
+//    int prob(vector<int>& nums){
+//        int index = 2; int sum=nums[0]; vector<int> allNum;
+//        while(index<nums.size()){
+//            if(index+2<nums.size()){
+//                if(nums[index+1]>nums[index]){
+//                    if(nums[index+1]<nums[index+2]){
+//                        sum+=nums[index]; allNum.push_back(nums[index]);
+//                    }else{sum+=nums[index+1]; allNum.push_back(nums[index+1]);}
+//                }else{sum+=nums[index];allNum.push_back(nums[index]);}
+//            }else if(index+1<nums.size()){
+//                if(nums[index+1]>nums[index]){
+//                    sum+=nums[index+1]; allNum.push_back(nums[index+1]);
+//                }else{sum+=nums[index];allNum.push_back(nums[index]);}
+//            }else{
+//                if(index-1>=0 && nums[index-1]>nums[index] && find(allNum.begin(),allNum.end(),nums[index-1])==allNum.end()){
+//                    sum+=nums[index];
+//                }else if(index-2>=0 && find(allNum.begin(),allNum.end(),nums[index-2])!=allNum.end()){
+//                        sum+=nums[index];
+//                }
+////                }
+//            }
+//            cout<<nums[index]<<" "<<sum<<endl;
+//            index+=2;
+//        }
+//        return sum;
+//    }
 };
 
 int main(){
@@ -50,6 +72,6 @@ int main(){
     vector<int> vec3{1,2,3,1};
     vector<int> vec4{2,7,9,3,1};
     vector<int> vec5{1,3,1};
-    cout<<s.prob(vec5)<<endl;
+    cout<<s.rob(vec5)<<endl;
     return 0;
 }
