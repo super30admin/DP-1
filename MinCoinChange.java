8e2f2bdb969b03b835a87da3da1c0544930ccf50
Arivoli AE <aehari2010@gmail.com>
2019-07-10 21:54:25 -0700

    Commit for Day 7 - Changes to Coin Change after session

--- MinCoinChange.java
+++ MinCoinChange.java
@@ -1,9 +1,7 @@
-// Time Complexity : O(n*n)
-// Space Complexity : O(n)
-// Did this code successfully run on Leetcode : No
+// Time Complexity : O(n*m) n is size of coin array and m is amount
+// Space Complexity : O(n*m) n*m storage
+// Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :
-// Unable to grasp dynamic intuition
-
 // Your code here along with comments explaining your approach
 
 
@@ -11,20 +9,40 @@ class MinCoinChange {
     
     public int coinChange(int[] coins, int amount) {
        
-        if(amount <= 0)
-            return 0;
-        
-        int result = Integer.MAX_VALUE;
-        for(int i=0;i<coins.length;i++){
-            if(coins[i] <= amount){
-                int subResult = coinChange(coins, amount-coins[i]);
-                if(subResult != Integer.MAX_VALUE && (subResult+1) < result){
-                    result = subResult+1;
-                }
-            }   
+        int[][] dp = new int[coins.length+1][amount+1];
+
+        //init 0 in first column
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 0;
         }
-       
-        return result == Integer.MAX_VALUE ? 0 : result;
+
+        //init max to 1st row
+        for(int j=1;j<dp[0].length;j++){
+            dp[0][j] = 9999;
+        }
+
+        //dp table
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                
+                //copy from top
+                if(j < coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i-1]] + 1);
+                }
+            }
+        }
+
+        int result = dp[coins.length][amount];
+        if(result >= 9999){
+            return -1;
+        }
+
+        return result;
+
+
     }
     public static void main(String[] args){
         System.out.println("MinCoinChange");
