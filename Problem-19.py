46c7fdf55b558abc6f82f34e6b60bc323791ea18
sindhu819 <sindu819@gmail.com>
2019-10-17 00:57:53 -0700

    Completed 19 & 20

---
+++ Problem-19.py
@@ -0,0 +1,78 @@
+# leet code -322
+# Dynamic Programming
+# passed all test cases
+# time complexity =O(N^2)
+#space complexity=O(N)
+#Approach - we need to create a matrix of size((number of coins+1 * (amount+1)). Let the first row be filled with infinity except first column in all the rows. Those values are filled with zero. Till the column reaches the coin denomination the matrix is filled with previous row values and then it compared with min value of (previous rowth column and the (current column -coin denomination))
+
+
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        i,j=0,0
+        rows,col=len(coins)+1,amount+1
+        dp=[[0 for i in range(col)] for j in range(rows)]
+        
+     
+        for i in range(rows):
+            dp[i][0]=0
+        for j in range(1,col):
+            dp[0][j]=99999
+        for i in range(1,rows):
+            for j in range(1,col):
+                if j<coins[i-1]: #extra row of zero added
+                    dp[i][j]=dp[i-1][j]
+                else:
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-coins[i-1]]+1)
+      
+        result=dp[rows-1][col-1]
+        
+        if result >=99999 :
+            return -1 
+        else :
+            return result
+        
+        
+# Recursive solution
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        return self.helper(coins,amount,0,0)
+    
+    def helper(self,coins,amount,i,min_num):
+        for i in range(len(coins)):
+        
+        #base cases
+            if (amount<0 or i> len(coins)):
+                return -1
+        
+            if (amount==0):
+                return min_num
+        
+        #case1 choosing the coin
+            case1=self.helper(coins,amount-coins[i],i,min_num+1)
+        
+        #case2  not choosing the coin
+            case2 = self.helper(coins,amount,i+1,min_num)
+        
+        if (case1 ==-1):
+            return case2
+        elif case2 ==-1:
+            return case1
+        else:
+            return min(case1,case2)
+
+    
+                    
+            
---
+++ Problem-20.py
@@ -0,0 +1,77 @@
+
+#leet code -198
+#recursion
+# Time complexity = O(2^N)
+#space complexity =O(N)
+# Got maximum recursive depth error for both the problems while solving through recursive
+
+class Solution(object):
+    def rob(self, nums):
+        return self.helper(nums,0,0)
+        
+    def helper(self,nums,i,max_amt):
+        for i in range(len(nums)):
+            
+            #edge case
+            if (i>=len(nums)) :
+                return max_amt
+            #choosen 
+            case1 = self.helper(nums,i+2,(max_amt+nums[i]))
+            
+             #not choosen
+            case2=self.helper(nums,i+1,max_amt)
+       
+        return max(case1,case2)
+
+
+# dynamic programming
+# Time complexity =o(n)
+# space complexity =o(n)
+# Dynamic programming approach - First we need to create the matrix of size (n,2). matrix[i][0] stores the maximum element from previous array and matrix[i][1] stores the sum of previous nonchoosen value and current number.
+
+
+class Solution(object):
+    def rob(self, nums):
+        if (nums == None or len(nums)==0 ): #edge cases
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        
+        
+        col,rows=2,len(nums)
+        i,j=0,0
+        dp=[[0 for i in range(col)] for j in range(rows)]
+        dp[0][0]=0  #base case
+        dp[0][1]=nums[0] #base case
+        
+        
+        for i in range(1,len(nums)):
+            for j in range(2):
+                if j==0:
+                    dp[i][0]=max(dp[i-1][0],dp[i-1][1])
+                else:
+                    dp[i][1]=dp[i-1][0]+nums[i]
+        return max(dp[i][0],dp[i][1])
+                
+#dynamic programming optimized solution : Same as above but storing the previous results.
+#time complexity =o(N)
+#space complexity =O(1)
+
+
+class Solution(object):
+    def rob(self, nums):
+        if (nums == None or len(nums)==0 ):
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        temp=0
+        choosen=0
+        not_choosen=0
+        
+        for i in range(len(nums)):
+            temp=choosen
+            choosen=not_choosen+nums[i]
+            not_choosen=max(temp,not_choosen)
+        return max(choosen,not_choosen)
+            
+    
