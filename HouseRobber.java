6b38d021e7f3190f0c12408be12c48a2e2e35946
Akhilesh Borgaonkar <aborgaonkar@onemarketnetwork.com>
2019-10-17 17:36:44 -0700

    added optimized solutions for both problems

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,23 +1,23 @@
 /*
 Author: Akhilesh Borgaonkar
 Problem: LC 198. House Robber (DP-1)
-Approach: Iterating through the input array and checking the maximum of (previous sum + current element) and (previous sum). This will give me the 
-    maximum amount and guarantee that no two continuous elements are added to avoid alarm.
+Approach: Using Dynamic programming approach to find the pattern of repeating sub-problems. Further optimized it by using just 2 variables to store
+    previous problem results.
 Time Complexity: O(n) where n is number of elements in array.
-Space complexity: O(1) constant. (not sure about this)
+Space complexity: O(1) constant.
 LC Verified.
 */
 
 class Solution {
     public int rob(int[] nums) {
-        int prevSum = 0, currSum = 0;
-        for(int i=0 ; i<nums.length; i++){
-            int tmp = currSum;                      //saving previous sum
-            if( (prevSum+nums[i]) > currSum )       //checking if the new sum by adding current element is greater than previous sum
-                currSum = prevSum+nums[i];          //if greater then add that element to final sum
-            prevSum = tmp;                          //make current sum as previous for next iteration
+        int chosen=0, notChosen=0;
+        
+        for(int i=0; i<nums.length; i++){               
+            int temp = chosen;                  
+            chosen = notChosen + nums[i];             
+            notChosen = Math.max(temp, notChosen);
         }
         
-        return currSum;
+        return Math.max(chosen, notChosen);
     }
 }
