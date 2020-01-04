e8380d2803d2680a6139b18e17d44eee63f2d4ae
BhaveshTadikonda <30873158+BhaveshTadikonda@users.noreply.github.com>
2020-01-04 17:46:24 -0500

    Please review house robber

---
+++ House robber.java
@@ -0,0 +1,27 @@
+// Time complexity O(nums)
+// Space complexity O(nums)
+
+class Solution {
+    public int rob(int[] nums) {
+        //edge case
+        if(nums == null || nums.length == 0) return 0;
+        int [][] dp = new int [nums.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i=1; i<nums.length;i++){
+            for ( int j=0; j < 2;j++){
+                if (j == 0){
+                    dp[i][j] = Math.max(dp[i-1][0],dp[i-1][1]);
+                } else {
+                    dp[i][j] = dp[i-1][0] + nums[i];
+                }
+                
+                
+                
+                
+            }
+        }
+        return Math.max(dp[dp.length-1][0],dp[dp.length-1][1]);
+        
+    }
+}
