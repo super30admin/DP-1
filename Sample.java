4c7d9f83cab6913546f0049ebf4d5e1dffea120e
poojakittur <gitpoojakittur@gmail.com>
2020-01-08 12:59:40 +0530

    Adding solutions to DP-1

--- Sample.java
+++ Sample.java
@@ -1,5 +1,13 @@
 // Time Complexity :
+//Problem_1 : O(n*m)
+// where n is coins and m is amount
+
+//Problem_2 : O(n) where n is number of houses
+
 // Space Complexity :
+//Problem_1 : O(n*m)
+//Problem_2 : O(n)
+
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
