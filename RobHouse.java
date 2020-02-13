82a06e6629c450132e16082ba5be76a549e34897
SowmyaDharani <sowmyadvn.92@gmail.com>
2020-02-13 09:35:24 -0800

    Update RobHouse.java

--- RobHouse.java
+++ RobHouse.java
@@ -1,5 +1,5 @@
-// Time Complexity : O(N)
-// Space Complexity : O(1)
+// Time Complexity : O(N) for both approaches where N is number of houses
+// Space Complexity : O(N) for approach-1 and O(1) for approach-2
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
