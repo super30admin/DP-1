489fcfc1776c1d59b135a9259eeb81d10ae6e17e
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-12 17:08:09 -0800

    DP-1 done

--- CoinChange.java
+++ CoinChange.java
@@ -1,36 +1,81 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(2^(M+N))?? for brute-force and O(MN) for dp where M is amount and N is coin array length 
+// Space Complexity : O(M+N) recursive call stack space and O(NM) auxilliary space for dp
+// Did this code successfully run on Leetcode : TLE for brute-force and successful for dp
+// Any problem you faced while coding this : Not sure if the time complexity for brute-force approach is right.
 
 
-// Your code here along with comments explaining your approach
+// ============= BRUTE FORCE - TIME LIMIT EXCEEDED =============
 
-// =================== TOP-DOWN =================================
+/*
+Algorithm:
+==========
+1. Generate the binary tree of all possible cases of picking and not picking every coin at every level
+2. Pick the minimum of picking and not picking in every scenario
+*/
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if(coins == null || coins.length == 0) return -1;
-        if(amount == 0) return 0;
-        
-        return coinChange(coins, amount, new int[amount]);
+        return helper(coins, amount, 0, 0);
     }
     
-    private int coinChange(int[] coins, int amount, int[] memo) {
-        if( amount < 0) return -1;
-        if( amount == 0) return 0;
+    private int helper(int[] coins, int amount, int index, int min) {
+        if( amount < 0 || index >= coins.length) return -1;
+        if( amount == 0 ) return min;  
         
-        if(memo[amount - 1] != 0) return memo[amount - 1]; //result found
-        int min = Integer.MAX_VALUE;
+        //coin picked, update amount and min
+        int picked = helper(coins, amount - coins[index], index, min + 1);
+
+        //coin not picked, update index
+        int notPicked = helper(coins, amount, index+1, min);
         
-        for(int coin: coins) {
-            int res = coinChange(coins, amount - coin, memo);
-            if( res >= 0 && res < min) 
-                min = res + 1;
+        if(picked == -1) return notPicked;
+        if(notPicked == -1) return picked;
+        
+        return Math.min( picked, notPicked);
+    }
+}
+
+// ========================== DP =========================================
+
+/*
+Algorithm:
+==========
+1. Update minimum change matrix based on set of conditions:
+ i. If column index less than coin value, update it with its above value in matrix
+ ii. Else, update the value with minimum of above value and minChange[i][j-d] where d is current denomination being considered
+2. If number of coins in the end greater than amount, implies it is impossible case. So, return -1, else return minChange
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0) return -1;
+        
+        // create denomination matrix
+        int[][] minChange = new int[coins.length + 1][amount + 1];
+        
+        // update first column with 0's for denomination 0
+        for(int j = 0; j <= coins.length; j++) 
+            minChange[j][0] = 0;
+        
+        // update matrix for denomination 0
+        for(int j = 1; j <= amount; j++) 
+            minChange[0][j] = amount + 1;
+        
+        
+        for(int i = 1; i < minChange.length; i++) {
+            for(int j = 1; j < minChange[0].length; j++) {
+                if(j < coins[i-1]) {
+                    minChange[i][j] = minChange[i-1][j];
+                }
+                else {
+                    minChange[i][j] = Math.min(minChange[i-1][j],minChange[i][j - coins[i-1]]+1);
+                }
+            }
         }
+        int result = minChange[minChange.length-1][minChange[0].length-1];
         
-        memo[amount - 1] = (min == Integer.MAX_VALUE)? -1 : min;
-        
-        return memo[amount - 1];
-        
+        if(result == amount+1) return -1;
+        return result ;
     }
 }
--- RobHouse.java
+++ RobHouse.java
@@ -4,12 +4,37 @@
 // Any problem you faced while coding this : no
 
 
+//================================= Approach-1 ================================================
+/*
+Algorithm:
+==========
+1. Keep track of max amount so far in an array as we iterate through the array of houses
+2. Max at the last house would be the max amount a robber can get
+*/
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        // array to keep track of max amount so far
+        int[] amount = new int[nums.length+1];
+        amount[1] = nums[0];
+        for(int i = 2; i <= nums.length; i++) {
+            // max of current sum and (previous sum + current value)
+            amount[i] = Math.max(amount[i-1],amount[i-2]+nums[i-1]);
+        }
+        
+        return amount[nums.length];
+    }
+}
+
+//================================= Approach-2 ================================================
+
 /*
 Algorithm:
 ==========
 1. As robber can steal only from alternate houses, we need to keep track of 2 sums, prev and current. 
-2. If currentSum is smaller than prevSum+newHouse money, implies currentSum has to be updated. 
-3. Old currsum would be new prevSum in that case. 
+2. If max is smaller than prevSum+newHouse money, implies max has to be updated. 
 */
 
 class Solution {
