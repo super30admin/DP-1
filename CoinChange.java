b1af4f59ba734e0f28726c2ee9c690fd1693f8c2
arcsoftech <arihant_Chhajed@yahoo.in>
2020-03-24 14:04:27 -0500

    Done Problem 1

--- CoinChange.java
+++ CoinChange.java
@@ -1,20 +1,40 @@
+// Time Complexity :O(m*n) where m is the number of coin and n is the amount
+// Space Complexity :O(m*n) where m is the number of coin and n is the amount
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
 class CoinChange {
-    public int coinChange(int[] coins, int amount) {
-        return helper(coins,0,amount,0);
+    public static int coinChange(int[] coins, int amount) {
+        // base condition
+        if(coins.length==0)
+            return -1;
+        int [][] dp = new int[coins.length+1][amount+1];
+        for(int i = 1;i <=amount;i++)
+        {
+            dp[0][i] = Integer.MAX_VALUE-7;
+        }
+        for(int i = 1;i <=coins.length;i++)
+        {
+            for(int j = 1;j<=amount;j++)
+            {
+                if(j-coins[i-1]<0)
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                }
+            }
+        }
+     
+        int result = dp[coins.length][amount];
+        return  result < (Integer.MAX_VALUE-7) ? result:-1;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(coinChange(new int[]{1,3,5},11));
     }
     
-    private static int helper(int[] coins,int coinIndex,int remainingAmount,int noOfCoin)
-    {
-        // base condition
-        if(remainingAmount<0 || coinIndex> coins.length-1) return -1;
-        if(remainingAmount==0) return noOfCoin;
-        //case 1
-        int coinChoosen = helper(coins, coinIndex,remainingAmount - coins[coinIndex],noOfCoin+1 );
-        //case 2
-        int coinNotChoosen = helper(coins, coinIndex+1, remainingAmount, noOfCoin);
-        //final result
-        if(coinChoosen==-1) return coinNotChoosen;
-        if(coinNotChoosen==-1) return coinChoosen;
-        else return Math.min(coinChoosen,coinNotChoosen);
-    }
+   
 }
