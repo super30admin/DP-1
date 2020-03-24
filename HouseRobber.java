e0ac5ba95e72eb6237560d1830dcfd7a2bdd9906
arcsoftech <arihant_Chhajed@yahoo.in>
2020-03-24 16:49:09 -0500

    Completed with O(1) complexity

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,24 +1,22 @@
 // Time Complexity :O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes 
 // Any problem you faced while coding this : No
 
-
 // Your code here along with comments explaining your approach
 
-class HouseRobber
-{
+class HouseRobber {
     public int rob(int[] nums) {
-        //base case
-        if(nums.length==0)
+        // base case
+        if (nums.length == 0)
             return 0;
-       int [] dp = new int[nums.length+1];
-        dp[1]= nums[0];
-       for(int i = 2;i<dp.length;i++)
-       {
-           dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i-1]);
-       }
-       return dp[nums.length];
+        if (nums.length == 1)
+            return nums[0];
+        nums[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i - 1], nums[i - 2] + nums[i]);
+        }
+        return nums[nums.length - 1];
     }
 
 }
