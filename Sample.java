f7246273fdc36eaf17bf300ab122fae2658661d1
Raju Datla <rajudatla@Rajus-MacBook-Pro.local>
2020-05-01 00:13:22 -0700

    Updated DP-1

--- Sample.java
+++ Sample.java
@@ -1,4 +1,29 @@
 
+//Recursion Solution
+//Time complexity: exponential 2 pow m+n
+//space complexity: o(m+n)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0)
+            return -1;
+        return helper(coins, amount, 0, 0);
+    }
+    private int helper(int[] coins, int amount, int index, int count){
+        //base case
+        if(index>=coins.length || amount < 0)
+            return -1;
+        if(amount == 0)
+            return count;
+        //logic
+        int case1 = helper(coins,amount, index+1,count);
+        int case2 = helper(coins,amount - coins[index], index, count+1);
+        if(case1 == -1)
+            return case2;
+        if(case2 == -1)
+            return case1;
+        return Math.min(case1,case2);
+    }
+}
 
 //Time complexity o(mn)
 //space complexity o(mn)
@@ -27,6 +52,28 @@ class Solution {
         if(dp[dp.length-1][dp[0].length-1] >= 9999)
             return -1;
         return dp[dp.length-1][dp[0].length-1];
+    }
+}
+
+
+//House Robbing
+//Time complexity: exponential 2 pow n
+//space complexity: o(n)
+//Recursion
+class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        return helper(nums, 0, 0);
+    }
+    private int helper(int[] nums, int amount, int index){
+        //base case
+        if(index>=nums.length)
+            return amount;
+        //logic
+        int case1 = helper(nums,amount, index+1);
+        int case2 = helper(nums,amount+nums[index], index+2);
+        return Math.max(case1, case2);
     }
 }
 
