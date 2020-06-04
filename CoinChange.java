14364b183e4e4f08c6fb41f27e4b4b14f64e9dfd
chirag <cr.tagadiya@gmail.com>
2020-06-03 23:33:04 -0400

    DP coin change 1 done

--- CoinChange.java
+++ CoinChange.java
@@ -41,4 +41,47 @@ class CoinChange {
         return Math.min(case1,case2);
     
     }
+
+/*
+    Dp Solution
+    Time Complexity : O(N*K)
+    Space Complexity : O(N*K) N =  amount K = no of coins
+   
+    Is worked on leetcode : YES
+    difficulty face : from brueforce to efficient solution 
+     we are mainting DP table because we have lots of overlapping subproblem
+    we dont need to calculate again and again, we will reuse already completed result to solve newer and
+    easier problem
+
+*/
+    public int coinChange2(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 ) return 0;
+        
+        int [] [] dp =  new int[coins.length + 1][ amount + 1 ];
+        
+        for(int i=0;i< dp.length; i++){
+            dp[i] [0] = 0;
+        }
+        for(int i = 1;i < dp[0].length; i++){
+            dp[0][i]  = 9999;
+        }
+        
+        for(int i=1;i < dp.length; i++){
+            for (int j = 1; j < dp[0].length; j++){
+                if (j < coins[i-1]){
+                    dp[i][j] =  dp[i-1] [ j];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j - coins[i-1]]+ 1);
+                }
+            }
+        }
+        
+        int result = dp[dp.length -1] [dp[0].length -1 ];
+        if( result  >= 9999){
+            return -1;
+        }else{
+            return result;
+        }
+    
 }
