1c4c84414319ad6a0f58590c02b2ff995c1aa657
OMKAR POTREKAR <39078516+omkarpotrekar@users.noreply.github.com>
2020-06-04 23:54:50 -0400

    Completed House RObber Problem

---
+++ houseRobber.cpp
@@ -0,0 +1,20 @@
+//Time Complexity - O(number of houses)
+//Space Complexity - O(2 * number of houses)
+//Runs successfuly on leetcode
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        int dp[nums.size()][2];
+        dp[0][0]=0;
+        dp[0][1]=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]=dp[i-1][0]+nums[i];
+        }
+        return max(dp[nums.size()-1][0],dp[nums.size()-1][1]);
+    }
+};
