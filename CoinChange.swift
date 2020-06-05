c451ba80f2c5a0619ccb952aa5ff3b9213cd36e8
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-06-05 07:25:41 -0400

    DP-1: DP tabulation approach.

--- CoinChange.swift
+++ CoinChange.swift
@@ -1,11 +1,11 @@
-// Time Complexity : (O)n*m
-// Space Complexity : (O)n*m
+// Time Complexity : (O)2n^m . n = number of coins. m = amount
+// Space Complexity : (O)2n^m
 // Did this code successfully run on Leetcode : timelimit exceeded
 
 
 //Recursive solution
 //Make two decision and get a number of coins needed to make the given amount
-//choose a coin or not choose a coins 
+//choose a coin or not choose a coins
 //then compare the min of two result and return the minimum
 
 class Solution {
@@ -39,10 +39,46 @@ class Solution {
             return case1
         }
         
-        if case1 < case2 {
-            return case1
-        }
-        
-        return case2
+        Swift.min(case1,case2)
     }
 }
+
+
+//DP tabular solution
+//Create a matrix to find all minimum number of coins to make a given amount from zero
+//Create 2d array with first row infinity and first column zero
+//set minimum number of coins needed to make the amount. There are two cases. case 1 is not to select a coin and case 2 is select a coin. For not select a coin get the previous(row) minimum value. For select a coin find the previously calculated minimum of coin to make a amount less than the current row amount.
+//Don't need to slect a coin if coin is greater than row amount
+//get min of zero case and selected case value
+
+class Solution {
+    func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+        //edge
+        if coins.count == 0 {
+            return 0
+        }
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: amount+1), count: coins.count+1)
+
+        //fill infinity for first row
+        for index in 1..<dp[0].count {
+            dp[0][index] = 9999
+        }
+        //fill zero for first dummy column
+        for index in 0..<dp.count  {
+            dp[index][0] = 0
+        }
+        for i in 1..<dp.count { // amount
+            for j in 1..<dp[0].count { // coin
+                if j < coins[i - 1] {
+                    dp[i][j] = dp[i-1][j]
+                } else {
+                    dp[i][j] = Swift.min(dp[i-1][j], dp[i][j - coins[i-1]] + 1) //min of zero case and selected case value
+                }
+            }
+        }
+        let result = dp[coins.count][amount]
+        return result >= 9999 ? -1 : result
+    }
+    
+}
+
