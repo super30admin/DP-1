17dd6fb947491e40141182ea1375082352c84195
Arpit Sharma <arpit317@gmail.com>
2020-07-16 16:37:26 -0700

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[][] dp = new int[nums.length+1][2];
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        
+        for(int i = 1; i < nums.length + 1; i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1] = nums[i-1]+dp[i-1][0];
+        }
+        
+        return Math.max(dp[nums.length][0], dp[nums.length][1]);
+    }
+}
