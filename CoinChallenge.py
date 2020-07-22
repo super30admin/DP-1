a307a11861e56370e6dd3dac30d610667ba64d06
Balaviknesh Sekar <bala21karthick@gmail.com>
2020-07-22 17:54:40 -0600

    Array 1 - completed

--- CoinChallenge.py
+++ CoinChallenge.py
@@ -8,10 +8,10 @@ class Solution:
 
         numberOfCoins = len(coins)
 
-        #we create a dynamic memory matrix, where in each execution we will refer the previous values
+        # we create a dynamic memory matrix, where in each execution we will refer the previous values
         dpMatrix = [[None for j in range(amount + 1)] for i in range(numberOfCoins + 1)]
 
-        #the number of ways of getting 0 value from 0 coins is 0
+        # the number of ways of getting 0 value from 0 coins is 0
         dpMatrix[0][0] = 0
 
         # the number of ways of getting 0 value from all other coins individually will be 0
@@ -22,14 +22,11 @@ class Solution:
         for j in range(1, amount + 1):
             dpMatrix[0][j] = 999999
 
-
-
         for i in range(1, numberOfCoins + 1):
 
             for j in range(1, amount + 1):
 
-
-                #if current value is less than or equal to the previous coin then optimal
+                # if current value is less than or equal to the previous coin then optimal
                 # solution will be using the previous coin ways of getting the value.
                 if coins[i - 1] > j:
 
@@ -37,8 +34,9 @@ class Solution:
 
                 else:
 
-                #else we compare the previous coin optimal way to reeach the target and current coin optimal way to reach  (target - current coin) value
-                #plus adding 1 value of current coin. And we use the min optimal ways
+                    # else we compare the previous coin optimal way to reeach the target and current coin optimal way to
+                    # reach  (target - current coin) value plus adding 1 value of current coin. And we use the min
+                    # optimal ways
 
                     dpMatrix[i][j] = min(dpMatrix[i - 1][j], dpMatrix[i][j - coins[i - 1]] + 1)
 
@@ -46,6 +44,3 @@ class Solution:
             return dpMatrix[-1][-1]
         else:
             return -1
-
-
-
--- HouseRobber.py
+++ HouseRobber.py
@@ -11,24 +11,21 @@ class Solution:
 
         homes = len(nums)
 
-        #Here we initalize the Dynamic Structure to refer the previous values in each execution
+        # Here we initialize the Dynamic Structure to refer the previous values in each execution
         dpMatrix = [[None for j in range(2)] for i in range(homes)]
 
-        #Here 0 will represent the house is not chosen
+        # Here 0 will represent the house is not chosen
         dpMatrix[0][0] = 0
 
         # Here 1 will represent the house is  chosen
         dpMatrix[0][1] = nums[0]
 
-
-
         for i in range(1, homes):
-
-            #if a house not chosen we will get the max value of previous home based on chosen or not
+            # if a house not chosen we will get the max value of previous home based on chosen or not
             dpMatrix[i][0] = max(dpMatrix[i - 1][0], dpMatrix[i - 1][1])
 
-            #if the house is chosen we will add the current hous value with the value of previous home when not chosen.
+            # if the house is chosen we will add the current hous value with the value of previous home when not chosen.
             dpMatrix[i][1] = nums[i] + dpMatrix[i - 1][0]
 
-        #finally we compare the last row and find the max value, which will be our max profit from the homes.
+        # finally we compare the last row and find the max value, which will be our max profit from the homes.
         return max(dpMatrix[-1][0], dpMatrix[-1][1])
