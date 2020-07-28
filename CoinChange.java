321ceb568e57ccac56e306195c11dbbd06d85501
Santosh Kallala <kallalasantosh@gmail.com>
2020-07-28 19:17:20 -0400

    DP-1

---
+++ CoinChange.java
@@ -0,0 +1,18 @@
+class CoinChange{
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+    //Time COmplexity :O( amount * number of coins)
+    //Space Complexity:O(Amount)
+}
