6103a70051d3af312fd6abfab4200da8c72a08f9
chinmayeetapaskar <chinmayee.tapaskar@gmail.com>
2020-08-26 22:39:27 -0700

    Complete Problem 1

---
+++ Problem1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Problem1 {
+
+	  public static int coinChange(int[] coins, int amount) {
+	        int max= amount+1;
+	        
+	        int[] dp = new int[amount+1];
+	        
+	        Arrays.fill(dp,max);
+	        
+	        dp[0]=0;
+	        
+	        for(int i=1;i<dp.length;i++){
+	            for(int j=0;j<coins.length;j++){
+	                if(coins[j]<=i){
+	                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+	                }
+	            }
+	        }
+	        
+	        return dp[amount]>amount ? -1: dp[amount];
+	    }
+	  
+	  public static void main(String[] args) {
+			
+			int[] nums= new int[] {1, 2, 5};
+			
+			
+			int res= coinChange(nums,11);
+			System.out.println("res : "+ res);
+		}
+}
