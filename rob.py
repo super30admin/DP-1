48c8c156eebb118b188141d6f5dc51a0b7e00643
udapy <udayphlk@gmail.com>
2020-09-23 19:36:49 -0700

    updating comments

--- rob.py
+++ rob.py
@@ -2,12 +2,12 @@ def rob(self, nums: List[int]) -> int:
     """
     #Brute force:
     only recursion - Create 2 lists even and odd. add them and return max(even,odd) as result
-    Polynomial in time.
+    Exponetial in time. 2^n.
 
-    #Dynamic Programming: recursion with memoization.
+    #Dynamic Programming: bottom up approach
     Similar problem as fibonacci numbers.
     Everytime we store sub problem and keep solving them ahead, which will speed up computations.
-    Every iteration adding even and odd items alternatively and keep storing sub-solution for next iteration,
+    Every iteration add even and odd items alternatively and keep storing sub-solution with max for next iteration,
     return max out of them.
 
     #Time complexity: O(n)
