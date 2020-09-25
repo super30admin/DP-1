f3bf8496926279ed9abd27eed7abda6136ef09ff
Jeevtesh Singh <50637812+jeevy222@users.noreply.github.com>
2020-09-24 22:04:06 -0400

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,19 @@
+def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [0]*(amount+1)
+        for i in range(1, amount+1):
+            if i in coins:
+                dp[i] = 1
+                continue
+            min_coins = float("inf")
+            for coin in coins:
+                if i-coin >= 0:
+                    min_coins = min(dp[i-coin], min_coins)
+            dp[i] = min_coins+1
+        if dp[-1] == float("inf"):
+            return -1
+        return dp[-1]
+        
+        #tc --> o(mn)
+        #sc --> o(n)
+        #m = length of the coins array
+        #n = amount
