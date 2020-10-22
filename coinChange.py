d7e2e023c684bc32b235bef5a4f1f6a005bc681e
GayatriHungund81194 <gayatri811.gh@gmail.com>
2020-10-22 00:47:32 -0700

    Coin change completeed

--- coinChange.py
+++ coinChange.py
@@ -1,19 +1,25 @@
-# Easy brute force solution would be iterate over all combinations and then find which arrays give minimum sum. Then return the minimum length array
+# As taught in class, the  solution here will be to design a grid and memoise the result to get solution
 # Time Complexity: O(n^2)
-# Space Complexity: O(n)
-from itertools import combinations 
+# Space Complexity: O(row * columns)
 def coinChange(coins,amount):
-    minLen = 999999
-    for i in range(len(coins)):
-        combo = list(combinations(coins,i))
-        print(combo)
-        for j in range(len(coins)-1):
-            if sum(combo[j]) == amount and minLen > len(combo[j]):
-                minLen = len(combo[j])
-        del combo
-    print(minLen)
+    if (coins is None or len(coins) is None):
+        return 0
+    dp = [[0 for l in range(len(coins)+2)] for m in range(amount + 2)]
+    for i in range(1,len(dp)):
+        dp[0][i] = amount + 1
+    for j in range(1,len(dp)):
+        for k in range(1,len(dp[0])):
+            if (k < coins[j-1]):
+                dp[j][k] = dp[j-1][k]
+            else:
+                dp[j][k] = min(dp[j-1][k], 1 + dp[j][j - coins[i-1]])
+    return dp[len(dp)-1][len(dp[0])-1]
+
+
+
+
 
 coins = [1,2,5]
-amount = 11
+amount = 13
 coinChange(coins,amount)
     
