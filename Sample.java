caf9e601334b67c64f13bc18d6ec05e589061c25
sourav singh <souravsingh@souravs-MacBook-Air.local>
2020-12-01 23:14:54 +0530

    DP problem

--- Sample.java
+++ Sample.java
@@ -1,7 +1,48 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(S*n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :Lot of difficulty
 
 
 // Your code here along with comments explaining your approach
+//Coin Change
+public class Solution {
+
+  public int coinChange(int[] coins, int amount) {
+    if (amount < 1) return 0;
+    return coinChange(coins, amount, new int[amount]);
+  }
+
+  private int coinChange(int[] coins, int rem, int[] count) {
+    if (rem < 0) return -1;
+    if (rem == 0) return 0;
+    if (count[rem - 1] != 0) return count[rem - 1];
+    int min = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      int res = coinChange(coins, rem - coin, count);
+      if (res >= 0 && res < min)
+        min = 1 + res;
+    }
+    count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+    return count[rem - 1];
+  }
+}
+
+//House Robber
+
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :Lot of difficulty
+
+
+public int rob(int[] num) {
+    int prevMax = 0;
+    int currMax = 0;
+    for (int x : num) {
+        int temp = currMax;
+        currMax = Math.max(prevMax + x, currMax);
+        prevMax = temp;
+    }
+    return currMax;
+}
