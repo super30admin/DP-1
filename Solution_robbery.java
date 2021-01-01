ca5333aaac511be92ed89d4c2cb45899fc1dfdc0
Sravanthi Pasumarthi <sravanthi.pasumarthi1@gmail.com>
2021-01-01 10:39:38 -0600

    Adding solutions for DP-1

---
+++ Solution_robbery.java
@@ -0,0 +1,14 @@
+class Solution {
+   public int rob(int[] num) {
+    int prevMax = 0;
+    int currMax = 0;
+    for (int x : num) {
+        int temp = currMax;
+        currMax = Math.max(prevMax + x, currMax);
+        prevMax = temp;
+    }
+    return currMax;
+}
+
+//Time Complexity: O(n) where n is no.of houses
+//Space Complexity: O(1)
