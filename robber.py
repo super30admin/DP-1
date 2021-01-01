e375db0fcfb6e549ef6d3aa440984dea02d10831
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-01-01 14:28:58 -0600

    Create robber.py

---
+++ robber.py
@@ -0,0 +1,8 @@
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this : 
+
+# Working on it..
+class Solution:
+    def rob(self, nums: List[int]) -> int:
