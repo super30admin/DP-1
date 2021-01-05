28ce22a7fa079187415dbf77be7ee72d9d718f6b
Sai Teja <saitejanagam@gmail.com>
2021-01-06 01:11:33 +0530

    completed DP-1

---
+++ Problem2.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/house-robber/
+//Time Complexity : O(N)
+//Space Complexity : O(N)
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1], nums[0]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+}
