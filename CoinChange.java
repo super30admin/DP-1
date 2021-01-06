848698a409ef5f28c61982609586c02d034b8b41
Raj <buddhdev.raj34@gmail.com>
2021-01-06 11:20:41 -0600

    DP-1 Implementation

---
+++ CoinChange.java
@@ -0,0 +1,27 @@
+// Time Complexity : 0(coins*amount)
+// Space Complexity : 0(amount)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Was able to think about recurssion as well as dp.
+// Had little difficulty in getting dp logic.
+
+class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount <= 0)
+            return 0;
+
+        int[] minNumber = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++) {
+            minNumber[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i && minNumber[i - coins[j]] != Integer.MAX_VALUE)
+                    minNumber[i] = Integer.min(minNumber[i], 1 + minNumber[i - coins[j]]);
+            }
+        }
+
+        if (minNumber[amount] == Integer.MAX_VALUE)
+            return -1;
+        else
+            return minNumber[amount];
+    }
+}
---
+++ HouseRobber.java
@@ -0,0 +1,23 @@
+// Time Complexity : 0(n)
+// Space Complexity : 0(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+class HouseRobber {
+    public int rob(int[] nums) {
+
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int skip = 0, take = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int temp = skip;
+            skip = Math.max(skip,take);
+            take = temp + nums[i];
+        }
+
+        return Math.max(skip, take);
+    }
+}
