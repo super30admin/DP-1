b425adce623829253d56a44eb3d4afab8d1c339d
Kapish Garg <kapish5467@gmail.com>
2021-01-27 12:46:19 -0800

    complete code

--- Problem1.java
+++ Problem1.java
@@ -32,3 +32,27 @@ class Solution {
         
     }
 }
+
+//Time complexity-O(m*n), space complexity - O(m*n), This approach compares the result of chossing the current coins vs not choosing current coin and adding the min to dp matrix.
+public int coinChange(int[] coins, int amount) {
+    int n=coins.length;
+    int m= amount;
+    int[][]dp = new int[n+1][m+1];
+    for(int i=0;i<n+1;i++)
+        dp[i][0]=0;
+    for(int j=1;j<m+1;j++)
+        dp[0][j]=amount+1;
+    
+    for(int i=1;i<n+1;i++)
+    {
+        for(int j=1;j<m+1;j++)
+        {
+            if(coins[i-1]>j)
+                dp[i][j]= dp[i-1][j];
+            else
+                dp[i][j]=Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+        }
+    }
+    return dp[n][m]==amount+1?-1: dp[n][m];
+}
+}
