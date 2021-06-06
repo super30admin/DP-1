bac45cadd1314eb994fd875525fc8f33f8a80924
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-05 20:14:52 -0700

    coin change using dp

--- CoinChange.java
+++ CoinChange.java
@@ -1,37 +1,39 @@
-//Time complexity - 2^n
-//Space complexity -
+//Time complexity - O(m*n)
+//Space complexity - O(m*n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
 
 class Solution {
-    public int coinChange(int[] coins, int amount) {
+   public int coinChange(int[] coins, int amount) {
       
      //using recursion method
       //null check
       if(coins.length == 0 || coins == null) {return 0;}
+      //dp matrix
+      int [][] dp = new int[coins.length+1][amount+1];
+      //fill in first column
+      for(int i=0;i<dp.length;i++) {
+        dp[i][0] = 0;
+      }
+      //fill the dummy row
+      for(int j=1;j<dp[0].length;i++) {
+        dp[0][j] = amount+1;
+      }
       
-       return helper(coins,amount,0,0);
+      for(int i =1; i<dp.length;i++) {
+        int (j=1; j< dp[0].length;j++) {
+          if(coins[i-1] > j) { //denom is less than amount
+            dp[i][j] = dp[i-1][j];
+            
+          } else {
+            dp[i][j] = Math.min(dp[i-1][j+1], 1+dp[i][j-coins[i-1]] )
+
+          }
+        } 
+      }
+      int result = dp[dp.length-1][dp[0].length-1];
+      if(result > amount) return -1;
+      return result;
+       
     }
-  
-     public static int helper (int coins[], int amount, int index, int minCoins) {
-       //base case 
-       //valid path
-       if(amount == 0) {
-         return minCoins;
-       }
-       
-       //non valid path
-       if(index == coins.length || amount < 0) {
-         return -1;
-       }
-       
-       //logic
-       //choose a coin
-       int case1 = helper(coins,amount-coins[index],index, minCoins+1);
-        // dont choose a coin
-       int case2 = helper(coins,amount,index+1, minCoins);
-       
-       // case returns -1
-       if(case1 == -1) return case2;
-       if(case2 == -1) return case1;
-       return Math.min(case1,case2);
-     }
 }
