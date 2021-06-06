59c0559793f9566451d32b0fd0c13860370ba608
Dhanashree Kamath <dhanashreekamath92@gmail.com>
2021-06-05 22:19:30 -0700

    house robbing using dp matrix

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,24 +1,23 @@
+// Time Complexity : O(n)
+// Space Complexity : O(m)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : No
+
 class Solution {
     public int rob(int[] nums) {
       //recursive approach
       //null case
       if(nums.length == 0 || nums == null) return 0;
-      return helper(nums,0,0);
+      int [][] dp = new int[nums.length][2];
+      dp[0][1] = nums[0]; 
+      for(int i =1; i< dp.length; i++) {
+        //not choose
+        dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
         
+        //choose
+        dp[i][1] = dp[i-1][0] + nums[i];
+      }
+        
+      return Math.max(dp[dp.length-1][0], dp[dp.length-1][1]);
     }
-  private int helper(int nums[], int index, int robbedAmount) {
-    //base
-    if(index >= nums.length) {
-      return robbedAmount;
-    }
-    
-    //logic
-    //choose to rob
-    int case1 =  helper(nums, index+2, robbedAmount+nums[index]);
-    
-    // dont choose
-    int case2 = helper(nums, index+1, robbedAmount);
-    
-    return Math.max(case1,case2);
-  }
 }
