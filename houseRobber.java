baf996aea516cab8d02f7e488f8cc2a1c3bd8ce4
Uma mahesh <umamaheshnov9@gmail.com>
2021-06-06 20:59:48 -0500

    rob houses

---
+++ houseRobber.java
@@ -0,0 +1,12 @@
+// TC : O(N)
+// SC : O(N)
+
+class Solution {
+    public int rob(int[] nums) {
+    int[] dp = new int[nums.length+2];
+    for(int i=0; i<nums.length; i++){
+        dp[i+2] = Math.max(dp[i]+nums[i], dp[i+1]);
+    }
+    return dp[dp.length-1];
+}
+}
