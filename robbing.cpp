8d534b7501e61f1c2bdc24402cd1b4ff5b506bd2
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-14 13:57:08 -0700

    Update robbing.cpp

--- robbing.cpp
+++ robbing.cpp
@@ -1,1 +1,32 @@
 //Max house robbings
+//This is an exhaustive approach - using recursion
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        
+        
+        return helper(nums, 0, 0);
+    }
+    
+    int helper(vector<int>& nums, int index, int max)
+    {
+        //base :
+        if(index>=nums.size())
+        {
+            return max;
+        }
+        
+        
+        //case1: choose
+        int case1= helper(nums, index+2, max+nums[index]);
+        
+        
+        //case2:dont choose
+        int case2= helper(nums, index+1, max);
+        
+        
+        return std::max(case1,case2);
+    }
+};
