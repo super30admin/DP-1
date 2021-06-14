ff54a06dd72c3cff8bd45d412a1f7704a25a68a1
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-14 14:45:51 -0700

    Update dp_robbing.cpp

--- dp_robbing.cpp
+++ dp_robbing.cpp
@@ -1,1 +1,23 @@
+//This code is DP approach
 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        
+        
+        vector<vector<int>> dp(nums.size(), vector<int>(2));
+        
+        dp[0][1]=nums[0];
+        for(int i=1; i<nums.size(); i++)
+        {
+            //case 0
+            dp[i][0]= std::max(dp[i-1][0], dp[i-1][1]);
+            //case1
+            dp[i][1]= dp[i-1][0]+nums[i];
+        }
+        return max(dp[dp.size()-1][0], dp[dp.size()-1][1]);
+    }
+    
+    
+};
