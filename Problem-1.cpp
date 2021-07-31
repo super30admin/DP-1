a172d32976475488a9b3bad8d0a98af61bd766ff
Durgesh Patil <patildurgesh1998@gmail.com>
2021-07-31 09:51:39 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -1,3 +1,31 @@
+//Time Complexity = O(2^N).   EXPONENTIAL
+//Space Complexity = O(1)
+//Where N is the length of the vector coins.
+class Solution {
+public:
+    int helper(vector<int> coins, int amount, int index, int mincoins)
+    {
+        //base case
+        if(amount==0)
+            return mincoins;
+        if(index>=coins.size() || amount<0)
+            return -1;
+        int case0 = helper(coins, amount, index+1, mincoins);
+        int case1 = helper(coins, amount-coins[index], index, mincoins+1);
+        
+        if(case0==-1)                                                           //if these two "IF" statement were not used then answer will always be -1.
+            return case1;
+        if(case1==-1)
+            return case0;
+        return min(case0,case1);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        return helper(coins, amount, 0, 0);
+        //here 0 is the index in coins vector.
+    }
+};
+
+
 //Time Complexity = O(M*N)
 //Space Complexity = O(M*N)
 //Where M is the amount and N is the length of the vector coins.
