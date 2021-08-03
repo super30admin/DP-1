d2a319fb6ca7d3aacfa0730cff6b9a872cc96367
Aditya Balasubramanyam <aditya.gnb@gmail.com>
2021-08-03 18:28:28 -0400

    Added Solutions

---
+++ Solution2.java
@@ -0,0 +1,22 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Solution2 {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int skip = 0, take = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            int temp = skip;
+            skip = Math.max(skip, take);
+            take = temp + nums[i];
+        }
+        return Math.max(skip, take);
+
+    }
+}
