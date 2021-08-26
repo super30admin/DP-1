180cdae839dea85ac815bdde73de7ed0962a2a75
Smriti0311 <smritigrao@gmail.com>
2021-08-26 16:58:45 -0400

    Completed house robber

--- houserobber.py
+++ houserobber.py
@@ -1,15 +1,23 @@
 # Time Complexity : O(N) as it is single pass
-# Space Complexity : O(1) 
-# Did this code successfully run on Leetcode : No, only partially
-# Any problem you faced while coding this : I am not getting the logic for all test cases`~
+# Space Complexity : O(N) for the array, it can also be done with 2 variables to make space O(1) 
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : I got stuck at the starting range of the for loop
 
 class Solution:
     def rob(self, nums) -> int:
-        pEven, pOdd = 0, 0
-        for i in range(len(nums)):
-            if i % 2 == 0:
-                pEven += nums[i]
-            else:
-                pOdd += nums[i]
-        return max(pEven, pOdd)
+        if len(nums) == 0 or nums is None:
+            return 0
+        
+        dp = [0 for i in range(len(nums) + 1)]
+        
+    
+        dp[-2] = nums[-1]  # if only one house to rob
+        
+        # as last one is set to 0 if no houses left to rob, 
+        # and last - 1 is set to last of nums as only one house to rob
+        for i in range(len(dp) - 3, -1, -1):
+            
+            dp[i] = max(dp[i+1], nums[i] + dp[i+2])  # max(dp[current + 1], nums[current] + (dp[current + 2]))
+        
+        return dp[0]
             
