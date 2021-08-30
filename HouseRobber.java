3661e0e1ad1a96d82f25ede9e1e984730367361e
Sai Krishna Wuyyuru <54835344+w-s-k387@users.noreply.github.com>
2021-08-29 23:29:08 -0500

    updated  DP-1

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,5 +1,17 @@
 package DP;
 
 public class HouseRobber {
+	 public int rob(int[] nums) {
+	        if(nums.length==0 || nums==null) return 0;
+	       int[][] dp= new int[nums.length][2];
+	        dp[0][1]=nums[0];
+	        for(int i=1;i<dp.length;i++){
+	            // not choose
+	            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]);
+	            // choose
+	             dp[i][1]=dp[i-1][0]+nums[i];
+	        }
+	        return Math.max(dp[dp.length-1][0],dp[dp.length-1][1]);
+	    }
 
 }
