31b2876fab38f01f3832228a1ea72ccc52a2e31d
hp2413 <hpatel130198@gmail.com>
2021-11-14 06:56:38 -0500

    DP-1 - Completed - updated

--- Problem_2.java
+++ Problem_2.java
@@ -7,12 +7,18 @@
 class Solution {
     public int rob(int[] nums){
         if( nums == null || nums.length == 0) return 0;
-        int [][] dp = new int [nums.length][2];
-        dp[0][1] = nums[0];
+        //int [][] dp = new int [nums.length][2];
+        //dp[0][1] = nums[0];
+        int skip = 0;
+        int take = nums[0];
         for(int i = 1; i < nums.length; i++){
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
-            dp[i][1] = dp[i-1][0] + nums[i];
+            int temp = skip;
+            //dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+            skip = Math.max(skip, take);
+            //dp[i][1] = dp[i-1][0] + nums[i];
+            take = temp + nums[i];
         }
-        return Math.max(dp[nums.length-1][0], dp[nums.length-1][1]);
+        //return Math.max(dp[nums.length-1][0], dp[nums.length-1][1]);
+        return Math.max(skip, take);
     }
 }
