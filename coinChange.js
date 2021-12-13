130227ae420c75ef697630bc1c0075f00e30b3f5
Salman Karim <salmankarimzindani@hotmail.com>
2021-12-13 17:39:59 -0600

    [coinChange] solves coin change using dp

--- coinChange.js
+++ coinChange.js
@@ -1,15 +1,21 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(m x n): for each m(coins) we iterate through n(amount + 1) items
+// Space Complexity : O(n): we use an aux array to store our numbers
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
-var coinChange = function (coins, amount, index) {
-  if (!index || amount - coins[index - 1] > amount) {
-    return Infinity;
+var coinChange = function (coins, amount) {
+  const dpArr = new Array(amount + 1).fill(Infinity);
+  dpArr[0] = 0;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 0; j < dpArr.length; j++) {
+      if (j >= coins[i]) {
+        let dpIndex = j - coins[i];
+        dpArr[j] = Math.min(dpArr[dpIndex] + 1, dpArr[j]);
+      }
+    }
   }
-
-  const excluded = coinChange(coins, amount, index - 1);
-  const included = coinChange(coins, amount - coins[index - 1], index - 1);
+  return dpArr[dpArr.length - 1] === Infinity ? -1 : dpArr[dpArr.length - 1];
 };
 
-coinChange([1, 2, 5], 11, 3);
+console.log(coinChange([2], 3));
