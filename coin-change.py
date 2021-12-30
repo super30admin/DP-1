90b407273154606c05812e0528d1f533c078e4a6
RuchiS26 <singhal.rchi@gmail.com>
2021-12-30 11:48:33 +0530

    Completed Coin Change

---
+++ coin-change.py
@@ -0,0 +1,43 @@
+# Time Complexity : n be the number of coins and m the amount : O(n *m)
+# Space Complexity : O(m)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Yes
+
+
+# Your code here along with comments explaining your approach
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+
+        # using the top down approach
+
+        def helper(coins, amount, dp=dict()):
+            if amount in dp:
+                return dp[amount]
+
+            if amount == 0:
+                return 0
+
+            if amount < 0:
+                return -1
+
+            # to keep track of all the remainder we need to pick the shortest of all
+            shortest = -1
+
+            for i in coins:
+                remainder = helper(coins, amount - i, dp)
+                if remainder != -1:
+                    curr = remainder + 1
+
+                    if shortest == -1 or curr < shortest:
+                        shortest = curr
+
+            dp[amount] = shortest
+            return shortest
+
+        return helper(coins, amount)
