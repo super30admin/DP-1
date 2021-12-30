94d030fcdc05f24729e09e4ee7fa7b764fc18633
DEEPAKRAJ DHARMAPURI SELVAKUMAR <deepak.rajds2014@gmail.com>
2021-12-29 21:09:24 -0500

    Ini soln.

---
+++ house-robber.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_len = len(nums)
+        if not nums_len:
+            return 0
+        
+        if nums_len <= 2:
+            return max(nums)
+        
+        last = nums[-1]
+        sec_l = max(last, nums[-2])
+        third_l = 0
+        
+        for i in range(nums_len - 3, -1, -1):
+            third_l = max(nums[i] + last, sec_l)
+            last = sec_l
+            sec_l = third_l
+        
+        return max(sec_l, third_l)
