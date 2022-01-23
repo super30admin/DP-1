3ba3cd1d4dbe7a68977246196fd93e25efdceea0
shirin <shirin32@gmail.com>
2022-01-23 02:15:06 -0800

    Updating unsaved changes in House Robber problem

--- HouseRobber.py
+++ HouseRobber.py
@@ -14,17 +14,32 @@ Constraints:
 
 """
 
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on leetcode : Yes
+# Any problem you faced while coding this : No
 
 from typing import List
 
-"""
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
+        if nums == None:
+            return 0
+        
+        n = len(nums)
+        skip = 0
+        take = nums[0]
+
+        for i in range(1, len(nums)):
+            temp = skip
+            skip = max(skip, take)
+            take = temp + nums[i]
+
+        return max(skip, take)
+
+
+        """
         maxNum = max(nums)
         print(maxNum)
         
