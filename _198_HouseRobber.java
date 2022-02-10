a00a911514a0f2e2eb48848868267beba39c10f1
amit singh <amitsingh5317@gmail.com>
2022-02-10 03:49:23 -0600

    added comments

--- _198_HouseRobber.java
+++ _198_HouseRobber.java
@@ -2,7 +2,7 @@
     	//time - o(n)
       //Ran on leeetcode successfully : Yes
       // Problem faced  : No
-    	//Approach :
+    	//Approach : linear memuization
 
       class Solution {
     public int rob(int[] nums) {
@@ -19,5 +19,21 @@
         }
 
         return arr[nums.length-1];
+    }
+
+ //Same solution as above, instead of taking extra array modify input array
+
+    public int rob(int[] nums) {
+
+        if(nums == null || nums.length ==0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for(int i=2; i<nums.length; i++){
+            nums[i] = Math.max(nums[i-1], nums[i] + nums[i-2]);
+        }
+
+        return nums[nums.length-1];
     }
 }
--- _322_CoinChange.java
+++ _322_CoinChange.java
@@ -1,4 +1,37 @@
 class Solution {
+  // space complexity - o(n*2)
+      	//time - o(n*2)
+        //Ran on leeetcode successfully : Yes
+        // Problem faced  : No
+      	//Approach ://top to down approach, using recursion and memoization
+	  int[][] dp;
+    public int coinChangeTopDown(int[] coins, int amount) {
+        dp = new int[coins.length][amount+1];
+        if(amount == 0) return 0;
+        if(coins == null || coins.length ==0) return -1;
+        int res =  coinChange(coins, amount, 0);
+        return  (res == Integer.MAX_VALUE-1) ? -1 : res;
+    }
+
+    public int coinChange(int[] coins, int amount, int index) {
+
+        if(amount == 0) return 0;
+        if(amount < 0 ) return Integer.MAX_VALUE-1;
+        if(index >= coins.length) return Integer.MAX_VALUE-1;
+        if(dp[index][amount] != 0) return dp[index][amount];
+
+        int excludeCurrCoin =  coinChange(coins, amount, index+1);
+        int includeCurrCoin =  1 + coinChange(coins, amount-coins[index], index);
+
+        dp[index][amount] = Math.min(excludeCurrCoin,includeCurrCoin);
+        return  dp[index][amount];
+    }
+
+    // space complexity - o(n*2)
+        	//time - o(n*2)
+          //Ran on leeetcode successfully : Yes
+          // Problem faced  : No
+    // bottom up approach using iteration and memoization
       public int coinChange(int[] coins, int amount) {
 
         if(amount == 0) return 0;
