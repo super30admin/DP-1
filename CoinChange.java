b5cd34c423142ca495c2a9af9180ad321dc71bf6
Prakash Kantheti <prakashkantheti@gmail.com>
2022-03-09 20:59:31 -0500

    DP - 1 Complete

---
+++ CoinChange.java
@@ -0,0 +1,32 @@
+class CoinChange {
+
+  /**
+   * 
+   * Time Complexity: 
+   */
+  public int coinChange(int[] coins, int amount) {
+    int[] memo = new int[amount];
+
+    return coinChangeHelper(coins, amount, memo);
+  }
+
+  int coinChangeHelper(int[] coins, int amount, int[] memo) {
+      if(amount == 0) { return 0; }
+      if(amount < 0) { return -1; }
+      
+      if(memo[amount-1] != 0) { return memo[amount-1]; }
+      
+      int min = Integer.MAX_VALUE;
+      
+      for(int coin : coins){
+          int reqCoins = coinChangeHelper(coins, amount - coin, memo) + 1;
+          if(reqCoins > 0 && reqCoins < min){
+              min = reqCoins;
+          }
+          
+          memo[amount-1] = (min == Integer.MAX_VALUE) ? -1 : min;
+      }
+      
+      return memo[amount-1];
+  }
+}
---
+++ Rob.java
@@ -0,0 +1,20 @@
+public class Rob {
+  public int rob(int[] nums) {
+    int[] memo = new int[nums.length];
+    
+    if(nums.length == 0) { return 0; }
+    if(nums.length == 1) { return nums[0]; }
+    if(nums.length == 2) { return Math.max(nums[0], nums[1]); }
+    if(nums.length == 3) { return Math.max(nums[0]+nums[2], nums[1]); }
+    
+    memo[0] = nums[0];
+    memo[1] = nums[1];
+    memo[2] = nums[0]+nums[2]; 
+    
+    for(int i = 3; i < nums.length; i++){
+        memo[i] = nums[i] + Math.max(memo[i-2], memo[i-3]);
+    }
+    
+    return Math.max(memo[nums.length-1], memo[nums.length-2]);
+  }
+}
