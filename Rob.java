2b9b023ba2ac0cfe336af8de4260f77e11e776fd
Prakash Kantheti <prakashkantheti@gmail.com>
2022-03-12 13:53:36 -0500

    Update Robber logic

--- Rob.java
+++ Rob.java
@@ -1,28 +1,52 @@
 public class Rob {
   /**
-   * 
-   * Time Complexity: O(n)
-   * Space Complexity: O(n)
-   * 
    * Any Problems solving this? No
    * Were you able to solve it on leetcode? Yes
    */
+  
+  /**
+  Approach - 1 
+  Maintain a DP matrix of the running sum with skip and take of each number.
+  
+  Time Complexity = O(n)
+  Space Complexity = O(n)
+  */
   public int rob(int[] nums) {
-    int[] memo = new int[nums.length];
-    
-    if(nums.length == 0) { return 0; }
-    if(nums.length == 1) { return nums[0]; }
-    if(nums.length == 2) { return Math.max(nums[0], nums[1]); }
-    if(nums.length == 3) { return Math.max(nums[0]+nums[2], nums[1]); }
-    
-    memo[0] = nums[0];
-    memo[1] = nums[1];
-    memo[2] = nums[0]+nums[2]; 
-    
-    for(int i = 3; i < nums.length; i++){
-        memo[i] = nums[i] + Math.max(memo[i-2], memo[i-3]);
-    }
-    
-    return Math.max(memo[nums.length-1], memo[nums.length-2]);
+      int[][] dp = new int [nums.length][2];
+      
+      dp[0][0] = 0;
+      dp[0][1] = nums[0];
+      
+      for(int i = 1; i < nums.length; i++){
+          dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+          
+          dp[i][1] = nums[i] + dp[i-1][0]; //take current and the skip of previous number
+      }
+      
+      return Math.max(dp[nums.length-1][0], dp[nums.length-1][1]); // At the last number, return max of the sum when you pick and don't pick the number.
   }
+
+  /**
+    Approach - 2
+    
+    Maintain only 2 variables:
+        skip : that counts the total you can get by skipping the current number.
+        take : that counts the total you can get by taking the current number.
+    
+    Time Complexity: O(n)
+    Space complexity: O(1)
+    */
+    
+    public int rob(int[] nums) {
+      int skip = 0, take = 0;
+      
+      take = nums[0];
+      for(int i = 1; i < nums.length; i++){
+          int temp_skip = skip;
+          skip = Math.max(skip, take);
+          take = nums[i] + temp_skip;
+      }
+      
+      return Math.max(skip, take);
+    }
 }
