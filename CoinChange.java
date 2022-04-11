f3cf2e9afe8029a415d05b76557ca25fc60c297a
skatkar <skatkar225@gmail.com>
2022-04-11 18:56:41 -0500

    Coin change

---
+++ CoinChange.java
@@ -0,0 +1,27 @@
+public class CoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0 ) return 0;
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+
+        for(int j=0; j< amount + 1; j++){
+            dp[0][j] = amount + 1;
+        }
+
+        for(int i=1; i< coins.length + 1;i++){
+            for(int j=1; j < amount + 1;j++){
+                // If the amount to achieve is smaller than the coin to use
+                if(j < coins[i - 1]){
+                    // copy from the above row of the same column
+                    dp[i][j] = dp[i - 1][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                }
+            }
+        }
+
+        if(dp[coins.length][amount] > amount + 1) return -1;
+        return dp[coins.length][amount];
+    }
+}
