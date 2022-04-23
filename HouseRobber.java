f8e196beff713ef6da44812540309e3e078b7a78
surajbodapati <lbodapat@buffalo.edu>
2022-04-23 17:09:13 -0400

    missing code added

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class HouseRobber {
+// Time Complexity : O(2N)== O(N) where N is the number of houses
+// Space Complexity : O(2N)== O(N) where N is the number of houses
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+// Your code here along with comments explaining your approach
+// When we chose to rob a home, take math.max of prev two dp rows, if we chose to ignore it, take dp[i-1][0] and add
+//curr house count.
+public class HouseRobber {
+    public int rob(int[] nums) {
+        if(nums.length==0 || nums==null) return 0;
+
+        int[][] dp=new int[nums.length][2];
+
+        dp[0][0]=0;
+        dp[0][1]=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]=dp[i-1][0]+nums[i];
+        }
+
+        return Math.max(dp[nums.length-1][0],dp[nums.length-1][1]);
+    }
 }
