12599e6854d8bb83ab7ac7b5aaeac8a2f0e44011
Hemanth Teja <42966988+HemanthTejaY@users.noreply.github.com>
2022-05-08 09:48:56 -0400

    Update houseRobber.cpp

--- houseRobber.cpp
+++ houseRobber.cpp
@@ -1,20 +1,65 @@
-//Time = O(N)
-//Space = O(1)
-
-//We track the best before the ith house is robbed and rested 
-//And we track the best before the ith house is rested and robbed
-//And if are robbing the current house, we get more loot, else we skip and loot remains same
+//Brute Force Solution 
+//TC = O(2^N) - exponential 
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        auto currentBest = 0, previousBest = 0;
-        for(auto num : nums)
-        {
-            auto temporary = currentBest;
-            currentBest = max(currentBest, previousBest + num);
-            previousBest = temporary;
+         if(nums.size()==0) return 0;
+        return helper(nums,0,0);
+    }
+    
+    int helper(vector<int>& nums, int index, int robbed){
+        //base
+        //Note: we are incrementing index by 2 in choose cases - so we can exceed the nums size too
+        if(index>=nums.size()) return robbed; 
+        //logic
+        int case1 = helper(nums,index+2,robbed+nums[index]); //choose
+        int case2 = helper(nums,index+1,robbed); //dont choose
+        return std::max(case1,case2);
+    }
+};
+
+
+//DP Solution - Time complexity optimzed by removing repeating subproblem computations
+//TC = O(N)
+//SC = O(N)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        if(size==0) return 0;
+        if(size==1) return nums[0];
+        vector<int> dp(size);
+        
+        dp[0]=nums[0];
+        dp[1]= std::max(nums[0],nums[1]);
+        
+        for(int i=2;i<size;i++){
+            dp[i] = std::max(dp[i-1], nums[i] + dp[i-2]);
         }
-        return currentBest;
+        return dp[size-1];
+    }
+};
+
+
+// DP Solution - We can further optimize space by using only 3 variables
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        if(size==0) return 0;
+        if(size==1) return nums[0];
+        
+        int twoBehind = nums[0]; //two steps behind
+        int oneBehind = std::max(nums[0],nums[1]); //one step behind
+        
+        for(int i=2;i<size;i++){
+            int current = std::max(oneBehind, nums[i] + twoBehind);
+            twoBehind = oneBehind;
+            oneBehind = current;
+        }
+        return oneBehind;
     }
 };
