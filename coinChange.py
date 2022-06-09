99d0c7114605b6e1e86d717de806163738f04a43
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-08 17:38:43 -0700

    q2

--- coinChange.py
+++ coinChange.py
@@ -1,7 +1,7 @@
 # Time Complexity : O(amount*len(coins)) O(m*n)
 # Space Complexity : O(1)
 # Did this code successfully run on Leetcode : yes
-# Any problem you faced while coding this : NO
+# Any problem you faced while coding this : Yes, had to lookup solution several times.
 
 
 # DP approch - Bottom up
