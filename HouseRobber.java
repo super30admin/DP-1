33eaf30d83712c32a27e2b2a72f129835727df40
Pavan Reddy Gottimukkula <46338498+pavanreddy21@users.noreply.github.com>
2022-07-05 10:46:34 +0000

    optimized the code

--- HouseRobber.java
+++ HouseRobber.java
@@ -1,16 +1,17 @@
 // Time Complexity : O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 
 class Solution {
     public int rob(int[] nums) {
-        int dp[][] = new int[nums.length][2];
         
-        dp[0][1] = nums[0]; 
+        int dontRobb = 0;
+        int robb = nums[0]; 
         for(int i=1;i<nums.length;i++){
-            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);
-            dp[i][1] = dp[i-1][0] + nums[i];
+            int prevDontRobb = dontRobb;
+            dontRobb = Math.max(dontRobb,robb);
+            robb = prevDontRobb + nums[i];
         }
-        return Math.max(dp[nums.length-1][0],dp[nums.length-1][1]);
+        return Math.max(dontRobb,robb);
     }
 }
