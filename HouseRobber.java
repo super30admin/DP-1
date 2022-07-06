e0280e56e697e1b41c82a29662749aab8ebb0fa8
tejasp27 <tejaspatil790@gmail.com>
2022-07-06 00:02:46 -0400

    "DP-1 Please review"

---
+++ HouseRobber.java
@@ -0,0 +1,17 @@
+//Time Complexity O(n)
+//Space Complexity O(n)
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[][] = new int[n][2];
+        dp[0][1] = nums[0];
+        
+        for(int i=1;i<n;i++){
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);
+            
+            dp[i][1] = dp[i-1][0]+nums[i];
+        }
+        return Math.max(dp[n-1][0],dp[n-1][1]);
+    }
+}
