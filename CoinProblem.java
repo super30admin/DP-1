91c090be26e70f8ab08b1a08b7fe8e957d461993
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-27 18:58:04 -0400

    Create CoinProblem.java

---
+++ CoinProblem.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins==null || coins.length==0) return 0;
+        int[][] matrix = new int[coins.length+1][amount+1];
+        matrix[0][0]=0;
+        for(int i= 0;i<amount+1;i++){
+            matrix[0][i] = amount+1;
+        }
+        
+        for(int i= 1;i<coins.length+1;i++){
+            for(int j=1;j<amount+1;j++){
+                if( j < coins[i-1])
+                    matrix[i][j] = matrix[i-1][j];
+                else
+                    matrix[i][j] = Math.min(matrix[i-1][j],matrix[i][j-coins[i-1]]+1);
+            }
+        }
+        int result= matrix[coins.length][amount];
+        if(result>amount)
+            return -1;
+        return result;
+    }
+}
