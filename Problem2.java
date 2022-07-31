8dc551ffa2876aa2a29da5ce95d17d6f4b996868
Erwin Palani <erwinpalani@Erwins-MacBook-Pro.local>
2022-07-31 11:23:43 -0500

    update Problem2 with optimized solution

--- Problem2.java
+++ Problem2.java
@@ -1,22 +1,30 @@
-// Time Complexity : O(2^n)
-// Space Complexity : O(n)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : No, encountered TLE
 // Any problem you faced while coding this : Not with brute force, working on the optimized/dp solution
 
 
 // Your code here along with comments explaining your approach
-// Every element has two options, either to include or exclude it. Call helper method recursively till array is exhausted and keep returning max out of including option and excluding option
+// Every element has two options, either to include or exclude it. 
 
 
 class Solution {
     public int rob(int[] nums) {
-        return helper(nums, 0);
+        
+        // TC: O(n)
+        // SC: O(1)
+        if(nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        
+        int prev = nums[0];
+        if(n == 1) return nums[0];
+        int curr = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++) {
+            int temp = curr;
+            curr = Math.max(curr, nums[i] + prev);
+            prev = temp;
+        }
+        return curr;
 	}
 
-	private int helper(int[] nums, int i) {
-		if (i >= nums.length) {
-			return 0;
-		}
-		return Math.max(helper(nums, i + 2) + nums[i], helper(nums, i + 1));
-    }
 }
