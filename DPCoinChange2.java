b871ee39cba615084a04d471bcfcbb5a74e6edc4
ajmalhsn <ajmalhsn@hotmail.com>
2022-07-30 22:55:12 -0400

    Updated Problem1 and Problem 2

--- DPCoinChange2.java
+++ DPCoinChange2.java
@@ -11,28 +11,24 @@ class DPCoinChange2 {
         System.out.println(obj.coinChange(coins,amount));
     }
     public int coinChange(int[] coins, int amount) {
-        if (coins.length == 0) return -1;
+        if (coins.length == 0) return 0;
         int[][] dp = new int[coins.length + 1][amount + 1] ;
-        dp[0][0] = 0;
-        // writing infinite value to the first row
-        for(int i =0; i < dp[0].length; i ++) {
-            dp[0][i] = amount + 3;
-        }
+        dp[0][0] = 1;
         // filling up the rest of the rows in dp matrix
         for(int i = 1; i < dp.length; i++) {
-            for(int j = 1; j < dp[0].length; j++) {
+            for(int j = 0; j < dp[0].length; j++) {
                 // amount is less than coin value then take the value which is calculated for the coin above that
                 if(j < coins[i-1]){
                     dp[i][j] = dp[i-1][j];
                 } 
                 else {
-                    // Take the min out of the one above cell or the cell value subtracting coins[i] 
-                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                    // Take the sum num of ways out of the one above cell or the cell value subtracting coins[i] 
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
                 }
             }
         }
         int result = dp[coins.length][amount];
-        if ( result > amount) return -1;
+        // if ( result == 0) return -1;
         return result;
     }
 }
