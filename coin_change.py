d711537b5cfbd9d68ca5b963919dc6ad8ac3856c
Siddhant Shah <siddhanta.shah@st.niituniversity.in>
2022-08-26 22:56:52 -0700

    Added house_robber problem

--- coin_change.py
+++ coin_change.py
@@ -6,7 +6,6 @@
 
 # // Your code here along with comments explaining your approach
 
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if coins== None or len(coins)==0:
@@ -17,13 +16,12 @@ class Solution:
         
         #Making the array of length plus 1 to accomodate the 0
         dp=[0] * (n+1)
-        #dp[0][0]=0
         
         for i in range(1,len(dp)):
             dp[i]=amount+1
         
-        for i in range(1,len(dp)):
-            for j in range(1,len(dp[0])):
+        for i in range(1,m+1):
+            for j in range(n+1):
                 if j<coins[i-1]:
                     dp[j]=dp[j]
                 else:
@@ -36,46 +34,4 @@ class Solution:
             return result
         
         
-
-# With One Array 
-
-# // Time Complexity : O(m*n)
-# // Space Complexity : O(m*n)
-# // Did this code successfully run on Leetcode : Yes
-# // Any problem you faced while coding this : Yes a bit tricky and confusing in python
-
-
-# // Your code here along with comments explaining your approach
-
-
-# class Solution:
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-#         if coins== None or len(coins)==0:
-#             return 0
-        
-#         m=len(coins)
-#         n=amount
-        
-#         #Making the array of length plus 1 to accomodate the 0
-#         dp=[0] * (n+1)
-#         #dp[0][0]=0
-        
-#         for i in range(1,len(dp)):
-#             dp[i]=amount+1
-        
-#         for i in range(1,m+1):
-#             for j in range(n+1):
-#                 if j<coins[i-1]:
-#                     dp[j]=dp[j]
-#                 else:
-#                     dp[j]=min(dp[j],1+dp[j-coins[i-1]])
-        
-#         result=dp[n]
-#         if result>amount:
-#             return -1
-#         else:
-#             return result
-        
-        
-
 
