08dd492c157b9f82e1e9f1d7e51cf7af71536044
Ketu Patel <58966891+Ketu25@users.noreply.github.com>
2022-09-26 18:53:38 -0400

    DP-1 Please Review

---
+++ S30 FAANMG Problem #21(Coin Change).py
@@ -0,0 +1,18 @@
+# time complexity = O(mn)
+# space Complexity = O(mn)
+# All test cases passed in Leetcode
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1]*(amount+1)
+        dp[0]=0
+        for a in range(1,amount+1):
+            for c in coins:
+                if a-c>=0:
+                    dp[a]=min(dp[a],1+dp[a-c])
+
+        if dp[amount]!=amount+1:
+            return dp[amount] 
+        
+        else:
+            return -1
+    
