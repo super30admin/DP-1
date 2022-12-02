dc56b7a9678f91de3b6eb11180972ded0fb35eac
TarunGambhi <118712515+TarunGambhi@users.noreply.github.com>
2022-12-02 15:34:24 +0530

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,17 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+//https://leetcode.com/problems/house-robber
+class Solution {
+    public int rob(int[] nums) {
+        int arr[] = new int[nums.length+1];
+        int total=0;
+        int prevMax=0;
+        arr[0]=0;
+        for(int i=1;i<arr.length;i++){
+            prevMax = (i-1>0) ? arr[i-2] : 0;
+            arr[i]= Math.max(arr[i-1],nums[i-1]+prevMax);            
+        }
+        return arr[nums.length];
+    }
+}
