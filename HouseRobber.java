8dbb4c8444b269b1dbe8790c7b86e44b71ecb3f8
TarunGambhi <118712515+TarunGambhi@users.noreply.github.com>
2022-12-03 10:54:34 +0530

    Update HouseRobber.java

--- HouseRobber.java
+++ HouseRobber.java
@@ -16,3 +16,17 @@ class Solution {
         return arr[nums.length];
     }
 }
+
+class Solution2 {
+    public int rob(int[] nums) {
+        if(nums==null || nums.length ==0 ) return -1;
+        if(nums.length ==1 ) return nums[0];
+        int arr[] = new int[nums.length];
+        arr[0] = nums[0];
+        arr[1] = Math.max(arr[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            arr[i]= Math.max(arr[i-1],nums[i]+arr[i-2]);
+        }
+        return arr[nums.length-1];
+    }
+}
