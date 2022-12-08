ace95707e85ffd33756f03c841006efc09cf81ae
Akshay Mutha <arm8@njit.edu>
2022-12-08 10:23:10 -0500

    completed DP-1 assignments

---
+++ assignments/src/House_Robber.java
@@ -0,0 +1,34 @@
+// Time: O(n), Space: O(1)
+
+public class House_Robber {
+    public int rob(int[] nums) {
+
+        int N = nums.length;
+
+        // Special handling for empty array case.
+        if (N == 0) {
+            return 0;
+        }
+
+        int robNext, robNextPlusOne;
+
+        // Base case initializations.
+        robNextPlusOne = 0;
+        robNext= nums[N - 1];
+
+        // DP table calculations. Note: we are not using any
+        // table here for storing values. Just using two
+        // variables will suffice.
+        for (int i = N - 2; i >= 0; --i) {
+
+            // Same as the recursive solution.
+            int current = Math.max(robNext, robNextPlusOne + nums[i]);
+
+            // Update the variables
+            robNextPlusOne = robNext;
+            robNext = current;
+        }
+
+        return robNext;
+    }
+}
