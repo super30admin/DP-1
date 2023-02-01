35241970ae7b3b9e0052cccc0862c1106dd02501
Akhil Reddy Duggasani <duggasan@usc.edu>
2023-01-31 23:50:03 -0800

    Bottom UP approach added

--- Sample.java
+++ Sample.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 
 // Time Complexity : O(N)
-// Space Complexity : O(N) for recursion
+// Space Complexity : O(N)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : None
 
 // Your code here along with comments explaining your approach
 
 /**
- * Based on the condition, we have two possibilities.
- * 1. Take the current value and skip the next one to jump to i+2th index.
- * 2. Ignore ith index and jump to i+1th index.
- * Take a dp array filled with -1 to store max value obtained till ith index.
- * Iterate from 0th index, see if dp[i] is already filled, that means dp[i]
- * already has highest value till i. If not -1, return dp[i] else take 
- * max(current index + i+2 to n index, i+1 to n index)
- * Whatever value is present in dp[n-1], that would be the highest value possible.
+ * Based on the condition, we have two possibilities. 1. Take the current value
+ * and skip the next one to jump to i+2th index. 2. Ignore ith index and jump to
+ * i+1th index. Take a dp array filled with -1 to store max value obtained till
+ * ith index. Iterate from 0th index, see if dp[i] is already filled, that means
+ * dp[i] already has highest value till i. If not -1, return dp[i] else take
+ * max(current index + i+2 to n index, i+1 to n index) Whatever value is present
+ * in dp[n-1], that would be the highest value possible.
  */
 class Sample {
 	public int rob(int[] nums) {
 		int[] dp = new int[nums.length];
 		Arrays.fill(dp, -1);
-		return helper(nums, dp, 0);
+		int ans = helper(nums, dp, 0);
+		return helperBU(nums);
 	}
 
 	public int helper(int[] nums, int[] dp, int i) {
@@ -31,5 +31,14 @@ class Sample {
 			return dp[i];
 		dp[i] = Math.max(nums[i] + helper(nums, dp, i + 2), helper(nums, dp, i + 1));
 		return dp[i];
+	}
+
+	public int helperBU(int[] nums) {
+		int[] dp = new int[nums.length + 1];
+		dp[nums.length - 1] = nums[nums.length - 1];
+		for (int i = nums.length - 2; i >= 0; i--) {
+			dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+		}
+		return dp[0];
 	}
 }
