43b4bfbdbd3d811adf8163d9e190fa587e593c83
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-03-18 16:16:33 -0700

    Update RobberyProblem.java

--- src/RobberyProblem.java
+++ src/RobberyProblem.java
@@ -26,20 +26,34 @@ class RobberySolution {
         return ans;
 
 
-// ***************************** Bottom-Up Approach *****************************
-//        final int n = nums.length;
-//        if (n == 0)
-//            return 0;
-//        if (n == 1)
-//            return nums[0];
-//        int[] dp = new int[n];
-//        dp[0] = nums[0];
-//        dp[1] = Math.max(nums[0], nums[1]);
+// // ***************************** Bottom-Up Approach *****************************
+//         int n = nums.length;
+//         if(n == 1) return nums[0];
+//         int curr = nums[0];
+//         int prev = Math.max(nums[0], nums[1]);
+//         for(int i=2; i<n; i++) {
+//             curr = Math.max(curr+nums[i], prev);
+//             int temp = curr;
+//             curr = prev;
+//             prev = temp;
+//         }
+//         return prev;
 //
-//        for (int i = 2; i < n; ++i)
-//            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-//
-//        return dp[n - 1];
+// // //       Time Complexity: O(n)
+// // //       Space Complexity: O(n)
+// //        final int n = nums.length;
+// //        if (n == 0)
+// //            return 0;
+// //        if (n == 1)
+// //            return nums[0];
+// //        int[] dp = new int[n];
+// //        dp[0] = nums[0];
+// //        dp[1] = Math.max(nums[0], nums[1]);
+// //
+// //        for (int i = 2; i < n; ++i)
+// //            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+// //
+// //        return dp[n - 1];
     }
     private int rob(int[] nums, int index)
     {
