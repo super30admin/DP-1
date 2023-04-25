9c6b3c381993a5d4a8b284f1d2f82baa37bd1f3e
Pranay Joshi <pranay@Pranays-MBP.attlocal.net>
2023-04-24 22:58:22 -0500

    Completed DP1 - Second Problem House robber

--- Sample.java
+++ Sample.java
@@ -25,3 +25,50 @@ class Solution {
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];   //check whether dp[amount] is equal to Integer.MAX_VALUE and returns the minimum number of coins needed to make up the amount 'amount' or '-1'
     }
 }
+
+## Problem2 (https://leetcode.com/problems/house-robber/)
+
+// 1st Approach
+
+// Time Complexity - 0(n) because the method uses a single loop that iterates through each element of the input array exactly once
+// Space Complexity - 0(n) because it creates a 2D array of size nums.length
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {              // base case
+            return 0;
+        }
+        int[][] dp = new int[nums.length][2];               // using a dp array with row nums.length and column 2
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {      
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);        
+            dp[i][1] = dp[i - 1][0] + nums[i];                             
+        }
+        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);    // return max of 0 and 1
+    }
+}
+
+
+// 2nd Approach
+
+// Time Complexity - 0(n) because the method uses a single loop that iterates through each element of the input array exactly once.
+// Space Complexity - 0(1) because algorithm does not use any additional data structures or arrays, it only stores two variables (skip and take) to keep track of the maximum values.
+
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {              // base case
+            return 0;
+        }
+        int skip = 0;                                       
+        int take = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {      
+            int temp = skip;                              // store skip in temp as it will get updated
+            skip = Math.max(skip, take);                  // maximum between the old skip value and take 
+            take = temp + nums[i];         
+        }
+        return Math.max(skip, take);
+    }
+}
