930d572d21975289f6894c45d0b4d4aaffbb1506
Sharvani T <60984556+sharvanitamaskar@users.noreply.github.com>
2023-05-03 13:21:45 -0400

    Update house_robber.java

--- house_robber.java
+++ house_robber.java
@@ -5,3 +5,20 @@
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int rob(int[] nums) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+
+        int skip=0;
+        int take=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int temp=skip;
+            skip=Math.max(skip,take);
+            take=temp+nums[i];
+        }
+        return Math.max(skip,take);
+    }
+}
