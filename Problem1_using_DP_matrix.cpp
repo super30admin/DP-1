2661e7e47ce9439be5e78d7cb2bb42056bf434a3
skapoor2024 <s.kapoor@ufl.edu>
2023-06-01 22:43:59 -0400

    updated:

--- Problem1_using_DP_matrix.cpp
+++ Problem1_using_DP_matrix.cpp
@@ -3,10 +3,33 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    vector<int> arr(8+1,0);
-    cout<<arr.size();
-    return 0;
-}
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        vector<vector<int>> arr(coins.size()+1,vector <int> (amount+1));
+        const int max_val{INT_MAX - 147483647};
+
+        for(int j{1};j<=amount;++j){
+            arr.at(0).at(j) = max_val;
+        }
+
+        for(int i{1};i<=coins.size();i++){
+            for (int j{};j<=amount;j++){
+                // case where values are less than the coin value
+                if(j<coins.at(i-1)){
+                    arr.at(i).at(j) = arr.at(i-1).at(j);
+                }
+                else if(j>=coins.at(i-1)){
+                    int val_1{1+arr.at(i).at(j-coins.at(i-1))};
+                    int val_2{arr.at(i-1).at(j)};
+                    arr.at(i).at(j) = (val_2<val_1)?val_2:val_1;
+                }
+            }
+        }
+        
+        if(arr.at(coins.size()).at(amount) == max_val) return -1;
+        return arr.at(coins.size()).at(amount);
+    
+    }
+};
