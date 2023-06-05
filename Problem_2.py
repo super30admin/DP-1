602200ca1a94357147ca836bd08dc75d2ef25ffe
vyankateshmisar <venkymisar@gmail.com>
2023-06-05 13:06:17 -0400

    with O(1) Space Complexity

--- Problem_2.py
+++ Problem_2.py
@@ -52,3 +52,27 @@ class Solution(object):
             dp[i]=max(nums[i]+dp[i-2],dp[i-1])
 
         return dp[len(nums)-1]
+    
+# Constant Space Complexity
+ 
+class Solution(object):
+    def rob(self, nums):
+        n=len(nums)
+        if n==0:
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        if len(nums)==2:
+            return max(nums[0],nums[1])
+
+        prev=nums[0]
+        curr=max(nums[0],nums[1])
+        
+
+        for i in range(2,len(nums)):
+            temp=curr
+            curr=max(curr,prev+nums[i])
+            prev=temp
+
+
+        return curr
