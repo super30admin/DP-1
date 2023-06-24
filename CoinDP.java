a67c9e4882eb1f0b48c72561526cdd262d06f772
Amey Sunil Kanunje <115860258+ameykanunje@users.noreply.github.com>
2023-06-24 18:12:14 -0400

    Add files via upload

---
+++ CoinDP.java
@@ -0,0 +1,28 @@
+// Time Complexity = o(m*n) Space Complexity=o(m*n)
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];
+        dp[0][0]=0;
+        for(int j=1;j<=amount;j++)
+        {
+            dp[0][j]=99999;
+        }
+        for(int i=1;i<=coins.length;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]>j){
+                    dp[i][j]= dp[i-1][j];
+
+                }
+                else
+                {
+                    dp[i][j] =Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                }
+            }
+        }
+        if(dp[coins.length][amount]>=99999) return -1;
+        else return dp[coins.length][amount];
+
+    }
+}
