6ae29ad81933662b2ef828e5d803dd3147c61fe0
Pratiksha Kapse <pratiksha@Pratikshas-MacBook-Pro.local>
2023-09-12 19:17:18 -0700

    add coin chnage and house robbing

--- dp_robbing.cpp
+++ dp_robbing.cpp
@@ -1,26 +1,39 @@
-//This code is DP approach
+//Time complexity: O(n)
+//Space complexity: O(1)
 
-//Time complexity : O(n)
-//Space complexity : O(1)
+//Approach of uncommented code: simply keep a var for skipping and taking a house, idea is that initially we store values of first house as skip and take 0 and value of house. From there we keep checking conditions where we either skip or take a house
+//skip: if you are skipping current house that means we take maximum from the past house's value 
+//take: if youre taking it that mean you take skipped value for prior house and add own houses value
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
-        
-        
-        vector<vector<int>> dp(nums.size(), vector<int>(2));
-        
+        //Time complexity: O(n)
+        //space complexity: O(n)
+    /*    int n=nums.size();
+        vector<vector<int>> dp(n, vector<int>(2));
+        dp[0][0]=0;
         dp[0][1]=nums[0];
-        for(int i=1; i<nums.size(); i++)
+
+        for(int i=1;i<n;i++)
         {
-            //case 0
-            dp[i][0]= std::max(dp[i-1][0], dp[i-1][1]);
-            //case1
-            dp[i][1]= dp[i-1][0]+nums[i];
+            dp[i][0]=max(dp[i-1][0], dp[i-1][1]);
+            dp[i][1]=nums[i]+dp[i-1][0];
         }
-        return max(dp[dp.size()-1][0], dp[dp.size()-1][1]);
+
+        return max(dp[n-1][0], dp[n-1][1]);*/
+
+        int n=nums.size();
+        int skip=0;
+        int take=nums[0];
+
+        for(int i=1;i<n;i++)
+        {
+            int temp=skip;
+            skip=max(skip,take);
+            
+            take=temp+nums[i];
+        }
+        return max(skip,take);
     }
-    
-    
 };
