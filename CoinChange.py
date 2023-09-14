dcefa83d579068982b7630b8727970917e11826c
Aprajita Jha <aprajita212@gmail.com>
2023-09-14 14:14:47 -0700

    completed DP-1

--- CoinChange.py
+++ CoinChange.py
@@ -1,10 +1,12 @@
 #TC : O(mn)
 #SC : O(mn)
+
 class Solution:
     def coinChange(self, coins, amount: int):
 
         if not coins:
             return 0
+        
         m, n = len(coins), amount
         dp = [[n+1 for i in range(n+1)] for j in range(m+1)]
 
@@ -17,6 +19,7 @@ class Solution:
                     dp[i][j]= dp[i-1][j]
                 else:
                     dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]])
+
         if dp[m][n] == n+1:
             return -1
         return dp[m][n]
