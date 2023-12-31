35686225260ccb28cd65f707c028797ba7df0fa0
God7wyn <jamesgodwynj@gmail.com>
2023-12-31 12:31:37 -0500

    Completed DP 1

---
+++ Problem_1.cpp
@@ -0,0 +1,29 @@
+/*
+ * Time Complexity : O(n*amount)
+ * Space Complexity : O(1)
+ * Did this code successfully run on Leetcode : Yes
+ * Any problem you faced while coding this : No
+*/
+
+#include <vector>
+
+class Solution {
+public:
+    int coinChange(std::vector<int>& coins, int amount) {
+        int dp[amount + 1];
+        const int MAX = amount + 1;
+
+        // fill(start_index, start_index + amount + 1, element_to_fill)
+        std::fill(dp, dp + amount + 1, MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i)
+                    dp[i] = std::min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] == MAX ? -1 : dp[amount];
+    }
+};
