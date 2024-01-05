f38acaf7e6ca42458aa7d08558cdeaf7688bc07e
Tanmay Singal <tanmaysingal@icloud.com>
2024-01-05 01:46:07 -0800

    House Robber.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,24 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int rob(int[] nums) {
+        
+        
+        int prev = 0, prevprev = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            int max = Math.max(nums[i]+prevprev, prev);
+            prevprev = prev;
+            prev = max;
+            
+        }
+        
+        return prev;
+    }
+}
